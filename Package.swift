3e2aa8d18d4ab260771fc8fbf40851f2d78c50dc
Bąk Jakub <kuba.bak.1997@gmail.com>
2025-08-31 08:19:25 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "LagRadarSDK",
+            name: "LagRadarKit",
             targets: ["LagRadarKit"])
     ],
     targets: [
