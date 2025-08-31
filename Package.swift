9138a394447dc6b097d6c1838da9376bfc02139c
Bąk Jakub <kuba.bak.1997@gmail.com>
2025-08-31 08:14:50 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     products: [
         .library(
             name: "LagRadarSDK",
-            targets: ["LagRadarSDK"])
+            targets: ["LagRadarKit"])
     ],
     targets: [
         .binaryTarget(
-            name: "LagRadarSDK",
+            name: "LagRadarKit",
             url: "https://github.com/user-attachments/files/22062890/LagRadarKit.xcframework.zip",
             checksum: "de2b80b5d720580403ca4859ab1899fda54811e9b821880b17695ae1465960e7"
         )
