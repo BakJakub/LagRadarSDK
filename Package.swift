fa9d845c1de14925ccb6145bf7a740a80614c1fc
Bąk Jakub <kuba.bak.1997@gmail.com>
2025-09-01 12:53:55 +0200

    Add LagRadarKit binary target for v1.0.1

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LagRadarKit",
-            url: "https://github.com/user-attachments/files/22071692/LagRadarKit.xcframework.zip",
-            checksum: "21323e1e6df15617e9524613d86690cf1d9d6920a04f4dc540b5db8589f98ec6"
+            url: "https://github.com/user-attachments/files/22076783/LagRadarKit.xcframework.zip",
+            checksum: "ab1a7827bb7430623aac8dbe3c0de22b632e59879010b95ec83427d94127d72e"
         )
     ]
 )
